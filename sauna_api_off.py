30156b4e0ef05ef777a8efe6cbf65a6ac6ad5168
Patrik Horemans <horemansp@hotmail.com>
2021-02-16 18:56:37 +0100

    Update sauna_api_off.py

--- sauna_api_off.py
+++ sauna_api_off.py
@@ -5,6 +5,8 @@ import huum_config
 
 url = 'https://api.huum.eu/action/home/stop'
 #enter your username and password from app
+usern = 'yourusername@something.bla'
+passw = 'thepassword of your app'
 x = requests.post(url, auth=(usern,passw))
 print(x.json())
 
