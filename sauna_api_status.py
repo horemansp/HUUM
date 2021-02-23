d7fba327629868f860b1fd469a08dbaeb7beff0c
Patrik Horemans <horemansp@hotmail.com>
2021-02-23 15:11:20 +0100

    Update sauna_api_status.py

--- sauna_api_status.py
+++ sauna_api_status.py
@@ -1,11 +1,9 @@
 import requests
-import huum_config
-#
 
 url = 'https://api.huum.eu/action/home/status'
 #enter your username and password from app
 usern = 'yourusername@something.bla'
-passwo = 'thepassword of your app'
+passw = 'thepassword of your app'
 x = requests.get(url, auth=(usern,passw))
 print(x.json())
 
